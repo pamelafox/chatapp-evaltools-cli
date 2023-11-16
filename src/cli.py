de4a1d0376a8b756b613cb3f91d97fec9bcdf25b
Pamela Fox <pamela.fox@gmail.com>
2023-11-16 12:22:29 -0800

    First commit

---
+++ src/cli.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+import typer
+
+import diff_app
+import summary_app
+
+app = typer.Typer()
+
+
+@app.command()
+def diff(
+    directory1: Path = typer.Argument(exists=True, dir_okay=True, file_okay=False),
+    directory2: Path = typer.Argument(exists=True, dir_okay=True, file_okay=False)
+    ):
+    diff_app.main(directory1, directory2)
+
+
+@app.command()
+def summary(
+    results_dir: Path = typer.Argument(exists=True, dir_okay=True, file_okay=False)
+    ):
+    summary_app.main(results_dir)
+
+
+def cli():
+    app()
